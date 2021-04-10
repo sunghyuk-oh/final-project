1911e0be6fbd127ec78a372eab6b3c60d342353f
Tim Oh <timoh@Tims-MacBook-Pro.local>
2021-04-10 14:03:06 -0400

    final commit

---
+++ routes/users.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs')
+
+/* GET users listing. */
+router.post('/login', function (req, res, next) {
+  const { email, password } = req.body;
+  var user_data = require('../database/user.json')
+  if (!user_data[email]) {
+    return res.status(400).send("user not found")
+  }
+
+  const current_password = user_data[email].password;
+
+  if (password !== current_password) {
+    return res.status(400).send("wrong password")
+  }
+
+  res.status(200).send(user_data[email]);
+  // validation to throw error if email does not exist
+
+  // throw new Error("user not found").message("user not found")
+});
+
+router.post('/register', function (req, res, next) {
+  const { first_name, last_name, email, password } = req.body;
+  var user_data = require('../database/user.json')
+  console.log("user data ", user_data[email], user_data);
+  if (!!user_data[email]) {
+    return res.status(400).send("user already exists, try login ")
+  }
+
+  if (!first_name
+    || !last_name
+    || !email
+    || !password) {
+    return res.status(400).send("required field is missing")
+  }
+
+  user_data[email] = {
+    password, first_name, last_name
+  }
+
+  fs.writeFileSync('./database/user.json', JSON.stringify(user_data))
+
+  res.send(user_data[email])
+});
+
+module.exports = router;
